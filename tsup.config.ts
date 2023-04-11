b3b28c8bb995dcf519f64ee53de2d922859588a3
Franck Wolff <frawolff@gmail.com>
2023-04-11 12:22:01 +0200

    Make react[-dom] and lodash external, clean false

--- tsup.config.ts
+++ tsup.config.ts
@@ -6,9 +6,10 @@ export default defineConfig({
     entry: ['src/index.ts'],
     dts: true,
     sourcemap: true,
-    clean: true,
+    clean: false, /* cleaning output directory causes issues with yarn link and webpack dev server */
     minify: env === 'release',
-    format: ['esm'],
+    format: [ 'esm' ],
+    external: [ 'react', 'react-dom', 'lodash' ],
     outExtension() {
         return {
             js: '.js',
